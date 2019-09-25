dd7f8a3b79aeb75ea66df747d036e31fde9b3762
camaymaryann123@gmail.com <camaymaryann@123@gmail.com>
2019-09-26 07:47:47 +0800

    fixed overlapping ui

--- controller.js
+++ controller.js
@@ -1,52 +1,50 @@
-$(document).ready(function() {
+$(function() {
     var socket = io();
-    var username = $('#username');
+    var username;
     var user;
-    var message = $("#message").val()
 
-
-    //set username
     $("#userForm").submit(function(event) {
         event.preventDefault();
         $("#home").hide();
         $("#main").show();
-        $(".card-footer").show();
-        socket.emit('user', username.val());
-        username = username.val();
-        username.val('');
-
+        $(".card-header").show();
+        socket.emit('user', $('#username').val());
+        username = $('#username').val();
+        $('#username').val('')
     });
 
-    // send messages
+    // $("#btnEnd").click(function(){
+    //     alert('end');
+    //     socket.emit('disconnect', $('#username').val());
+    //     $("#home").show();
+    //     $("#main").hide();
+    //     $(".card-footer").hide();
+    //     $('.messages').empty();
+    // });
+
     $('#messageForm').submit(function(event) {
         event.preventDefault();
-        alert("hello")
         socket.emit('chat message', $('#message').val());
         $('#message').val('');
-
+        return false;
     });
-
-    //position of messages
     socket.on('chat message', function(msg) {
-        user = msg.split(":")
-        if (username == user[0]) {
-            $('.messages').append($('<div class=" float-left bg-light myMessage"><br><br>').val(msg));
+        msg = msg.split(":")
+        if (username == msg[0]) {
+            $('.messages').append($('<div class="myMessage bg-light">').append("<span>" + msg[0]).append("<br><p>" + msg[1]));
             window.scrollTo(0, document.body.scrollHeight);
         } else {
-            $('.messages').append($('<div  class="bg-success otherMessage"><br><br>').val(msg));
+            $('.messages').append($('<br><div class="otherMessage">').append("<span>" + msg[0]).append("<br><p>" + msg[1]));
             window.scrollTo(0, document.body.scrollHeight);
         }
 
     });
 
-    //handle new user connection
     socket.on('user', function(info) {
         var name = info.split(" ")
         $('#users').append($('<li>').text(info));
         $('#online-users').append('<tr><td>' + name[0] + '</td></tr>');
     });
-
-
 
     // socket.on('disconnect', function (msg) {
     //     $('#users').append($('<li>').text(msg));
@@ -60,12 +58,5 @@ $(document).ready(function() {
     //     }
     //   });
 
-    // $("#btnEnd").click(function(){
-    //     alert('end');
-    //     socket.emit('disconnect', $('#username').val());
-    //     $("#home").show();
-    //     $("#main").hide();
-    //     $(".card-footer").hide();
-    //     $('.messages').empty();
-    // });
+
 });
